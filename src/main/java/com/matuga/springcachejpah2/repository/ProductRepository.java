941cecf6e108370a968eda4da5a931bec8af81e1
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-03-10 01:45:37 +0530

    [SpringCaching]-#3- Added Redis connection for CURD ops

--- src/main/java/com/matuga/springcachejpah2/repository/ProductRepository.java
+++ src/main/java/com/matuga/springcachejpah2/repository/ProductRepository.java
@@ -3,5 +3,7 @@ package com.matuga.springcachejpah2.repository;
 import com.matuga.springcachejpah2.model.Product;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {}
--- src/main/java/com/matuga/springcachejpah2/service/ProductService.java
+++ src/main/java/com/matuga/springcachejpah2/service/ProductService.java
@@ -3,34 +3,43 @@ package com.matuga.springcachejpah2.service;
 import com.matuga.springcachejpah2.model.Product;
 import com.matuga.springcachejpah2.repository.ProductRepository;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+  public static final String HASH_KEY = "Product";
   @Autowired ProductRepository productRepository;
+
+  @Autowired private RedisTemplate template;
 
   // Find All
   public List<Product> findAll() {
-    return productRepository.findAll();
+    //    return productRepository.findAll();
+    return template.opsForHash().values(HASH_KEY);
   }
 
   // Create Product
   public Product save(Product product) {
-    return productRepository.save(product);
+    //    return productRepository.save(product);
+    template.opsForHash().put(HASH_KEY, product.getId(), product);
+    return product;
   }
 
   // Get by Id
-
+  //  @Cacheable(key = "#productId", value = "Product")
   public Product findByProductId(Integer productId) {
-    Optional<Product> productOptional = productRepository.findById(productId);
-    return productOptional.get();
+    //    productRepository.findById(productId);
+    System.out.println("called findProductById() from DB");
+    return (Product) template.opsForHash().get(HASH_KEY, productId);
   }
 
   // Delete By Id
   public String deleteByProductId(Integer productId) {
-    productRepository.deleteById(productId);
+    //    productRepository.deleteById(productId);
+    template.opsForHash().delete(HASH_KEY, productId);
     return "ProductID - " + productId + " successfully deleted";
   }
 }
